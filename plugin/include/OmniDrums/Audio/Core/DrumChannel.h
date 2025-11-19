f7e58d2b42454d924985d9c72e4b7fcaa86fc75e
hsetlik <haydensetlik@gmail.com>
2025-11-19 15:13:53 -0700

    DrumChannel implementation

--- plugin/include/OmniDrums/Audio/Core/DrumChannel.h
+++ plugin/include/OmniDrums/Audio/Core/DrumChannel.h
@@ -1,1 +1,21 @@
-# pragma once
+#pragma once
+#include "SampleCache.h"
+
+class DrumChannel {
+private:
+  SamplePlayer* player = nullptr;
+  bool isPlaying = false;
+  bool isCrossFading = false;
+  size_t currentIndex = 0;
+  size_t crossFadeIndex = 0;
+  float currentVelocity = 0.0f;
+  float nextVelocity = 0.0f;
+  float lastOutput = 0.0f;
+
+public:
+  const int channel;
+  DrumChannel(int c);
+  void tick();
+  float getOutput() const { return lastOutput; }
+  void trigger(float velocity);
+};
--- plugin/source/DrumChannel.cpp
+++ plugin/source/DrumChannel.cpp
@@ -1,1 +1,44 @@
+#include "OmniDrums/Audio/Core/DrumChannel.h"
+
+#define CROSS_FADE_SAMPLES 100
 //===================================================
+DrumChannel::DrumChannel(int c) : channel(c) {}
+
+void DrumChannel::tick() {
+  if (!isPlaying)
+    return;
+  if (!isCrossFading) {
+    lastOutput = player->getValue(currentIndex);
+    ++currentIndex;
+    isPlaying = currentIndex < player->lengthInSamples();
+  } else {
+    auto prevIdx = juce::jmin(currentIndex + crossFadeIndex,
+                              player->lengthInSamples() - 1);
+    float ratio = (float)crossFadeIndex / (float)CROSS_FADE_SAMPLES;
+    lastOutput = flerp(player->getValue(prevIdx),
+                       player->getValue(crossFadeIndex), ratio);
+    ++crossFadeIndex;
+    // check if we're finished cross fading yet
+    if (crossFadeIndex >= CROSS_FADE_SAMPLES) {
+      currentIndex = crossFadeIndex;
+      currentVelocity = nextVelocity;
+      isCrossFading = false;
+    }
+  }
+}
+
+void DrumChannel::trigger(float velocity) {
+  if (player == nullptr)
+    return;
+  if (!isPlaying) {
+    isPlaying = true;
+    isCrossFading = false;
+    currentVelocity = velocity;
+    currentIndex = 0;
+    lastOutput = 0.0f;
+  } else {
+    isCrossFading = true;
+    crossFadeIndex = 0;
+    nextVelocity = velocity;
+  }
+}
